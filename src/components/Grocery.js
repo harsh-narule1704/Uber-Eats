b050f47317996f230d76a7ca438a271e52ae2f75
harsh-narule1704 <harshnarule7931@gmail.com>
2025-08-11 23:24:30 +0530

    ep09 part2

---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+
+const Grocery = () => {
+    return (<h1>
+                Grocery Online Store, and we have a lot of child 
+                components inside this web page !!!
+            </h1>
+    );
+};
+
+export default Grocery;
